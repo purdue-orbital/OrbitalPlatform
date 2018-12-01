53551423ee235d45194403d78068880249e805b5
Thomas Tromp <thomasctromp@gmail.com>
2018-12-01 11:45:37 -0500

    Fixed spacing

--- examples/readData/readData.ino
+++ examples/readData/readData.ino
@@ -27,16 +27,16 @@ void setup(){
 		}while(success == false);
 	}
 	Serial.println("IMU Enabled");
-  plat.printPins();
+	plat.printPins();
 	digitalWrite(LED_BUILTIN, HIGH);	 // Enable built in LED to show that the device is enabled properly
 }
 
-Vector accel;  // m/s^2
-Vector gyro;   // rads/s
-Vector mag;    // uT
+Vector accel;	// m/s^2
+Vector gyro;	 // rads/s
+Vector mag;		// uT
 
 void loop(){
-  
+	
 	plat.readData();
 	
 	plat.getAccel(&accel);
@@ -49,25 +49,25 @@ void loop(){
 	Serial.print(" ");
 	Serial.print(gyro.z, 6);
 	Serial.print("\n");
-  
-  
+	
+	
 	if(gyro.z < -0.5){ // If the device is rotating counter-clockwise @ more than 0.5 rads/s turn on solenoid 1 (pin 4)
 		plat.setSolenoidActive(1, true);
 
-    plat.setSolenoidActive(2, false);
-    plat.setSolenoidActive(3, false);
-    
+		plat.setSolenoidActive(2, false);
+		plat.setSolenoidActive(3, false);
+		
 	}else if(gyro.z > 0.5){ // If the device is rotating clockwise @ more than 0.5 rads/s turn on solenoid 2 (pin 6)
-    plat.setSolenoidActive(3, true);
-    
+		plat.setSolenoidActive(3, true);
+		
 		plat.setSolenoidActive(1, false);
-    plat.setSolenoidActive(2, false);
+		plat.setSolenoidActive(2, false);
 	}else{ // If the device is rotating less than 0.5 rads/s turn on solenoid 2 (pin 5) NOTE: This is connected to a LED instead of an actual solenoid
-    plat.setSolenoidActive(2, true);
-    
-    plat.setSolenoidActive(1, false);
-    plat.setSolenoidActive(3, false);
-    
+		plat.setSolenoidActive(2, true);
+		
+		plat.setSolenoidActive(1, false);
+		plat.setSolenoidActive(3, false);
+		
 	} 
 
 	delay(50);
